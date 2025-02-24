43350ef1b32b658810a84e77fd0d4d908c68857f
12pounce89 <astorrs27@gmail.com>
2025-02-24 13:13:57 -0500

    uncommented some code and improved quick cmd

--- schedule_task.py
+++ schedule_task.py
@@ -13,30 +13,6 @@ def create_text_file():
         file.write("You've been pranked")
 
 
-def create_quick_cmd():
-    launcher_code = """
-import os
-os.system('start cmd')
-""" # Code within the created file
-    with open(os.path.join(DOCUMENTS_PATH, "quick_cmd.py"), "w") as file:
-        file.write(launcher_code)
-    os.system(f"python -m PyInstaller --onefile \"{os.path.join(DOCUMENTS_PATH, "quick_cmd.py")}\"") # Turns the created Python file into an executable
-    # Moves the file to the documents folder
-    dist_executable = Path("dist") / "quick_cmd.exe"
-    destination = os.path.join(DOCUMENTS_PATH, "quick_cmd.exe")
-    if dist_executable.exists():
-        shutil.move(str(dist_executable), str(destination))
-    else:
-        pass
-    # Remove created folders and files to keep things clean
-    for folder in ["dist", "build"]:
-        if Path(folder).exists():
-            shutil.rmtree(folder)
-    spec_file = Path("quick_cmd.spec")
-    if spec_file.exists():
-        spec_file.unlink()
-
-
 def schedule_task():
     interval = "MINUTE"
     repeat_interval = "1"
@@ -55,7 +31,7 @@ def schedule_task():
     # Run executable
     command = [
         "schtasks", "/create", "/tn", TASK_NAME,
-        "/tr", f'{os.path.join(DOCUMENTS_PATH, "quick_cmd.exe")}',
+        "/tr", f'cmd.exe',
         "/sc", interval, "/mo", repeat_interval,
         # "/rl", "HIGHEST",  # Run with elevated privileges (only works if run as administrator)
         "/f"  # Force update if task already exists
@@ -73,5 +49,4 @@ def schedule_task():
 
 if __name__ == "__main__":
     # create_text_file()
-    # create_quick_cmd()
     schedule_task()
